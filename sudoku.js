f2d6ceddd7c209e872ecad6edd0378e579e7ceac
Alix Virot <flying.cat@mac.com>
2022-10-23 22:48:18 +0200

    working on selected numbers and display

--- sudoku.js
+++ sudoku.js
@@ -35,7 +35,7 @@ window.onload = function () {
 // Create and populate the board with digits in js
 
 function setGame() {
-  // digits 1 -> 9 with a loop
+  // digits :  1 -> 9 with a loop
   for (let i = 1; i <= 9; i++) {
     // Create 9 <div id= i class ="number">
     let number = document.createElement("div");
@@ -46,17 +46,56 @@ function setGame() {
     // Add  a class for style
     number.classList.add("number");
 
+    number.addEventListener("click", selectNumber);
+
     // Add it to the digits div
     document.getElementById("digits").appendChild(number);
   }
 
-  // BOARD 9 X 9
+  // board :  9 X 9
   for (let row = 0; row < 9; row++) {
     for (let col = 0; col < 9; col++) {
       let tile = document.createElement("div");
       tile.id = row.toString() + "-" + col.toString();
+
+      if (row == 2 || row == 5) {
+        tile.classList.add("horizontal-line");
+      }
+      if (col == 2 || col == 5) {
+        tile.classList.add("vertical-line");
+      }
+
+      tile.addEventListener("click", selectTile);
       tile.classList.add("tile");
       document.getElementById("board").append(tile);
     }
+  }
+}
+
+function selectNumber() {
+  // "this" refers to the div itself, the one you clicked, "number" of "digits"
+  // We want to check to toggle on/off.
+  // If numSelected is not equal to null bc we alreday selected one,
+  // we remove that class, so it removes the lightskyblue background
+  // and we updated it to the new number selected + add background to it.
+  if (numSelected != null) {
+    numSelected.classList.remove("number-selected");
+  }
+  numSelected = this;
+  numSelected.classList.add("number-selected");
+}
+
+// When we select a number and click on a tile, we want this tile display the number.
+// If a number is selected we are going to "this" (refering to the tile itself)
+// .innertext  equal to num selected id. So create a click handeler.
+// but we don't want to overide/change the number if we select an other one...
+// so conditionnal if this not  = to blank string, we want to return. it means exit the function and do nothing else
+
+function selectTile() {
+  if (numSelected) {
+    if (this.innerText != "") {
+      return;
+    }
+    this.innerText = numSelected.id;
   }
 }
